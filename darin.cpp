842c7934236107f143a2728ba32c8e79b26d93c6
dpnelson <dpnelson@memphis.edu>
2015-02-24 18:56:26 -0600

    With Comment Only

--- darin.cpp
+++ darin.cpp
@@ -12,5 +12,14 @@
 using namespace std;
 
 int main () {
+    
+    cout << "I was able to compile this code using the HPC at the University of Memphis.\n"
+         << "When I compiled it there, it did not produce any warning message.\n"
+         << "The HPC uses a GNU C++ compiler that can be considered a good up-to-date standard.\n"
+         << "I also version-controlled this code using git, and used a remote repository hosted by github.\n"
+         << "If I can do this, so can you!!!\n"
+         << "I am so cool, that I was able to write a code that produces the first M numbers of the Fibonacci sequence\n\n"
+         << "Here they are:\n\n";
+    
     return 0;
 }
